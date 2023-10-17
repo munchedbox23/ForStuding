1ab77286078d115d9f25cca67ffb7c2db63741e7
Egor <egor032324@mail.ru>
2023-10-17 22:27:42 +0300

    Add Homework №2

---
+++ Homework #2/My project/Assets/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject Capsule;
+    public GameObject Cube;
+    
+    public void onClick()
+    {
+        if (Capsule.activeSelf)
+        {
+            Capsule.SetActive(false);
+            Cube.SetActive(false);
+        }
+        else
+        {
+            Capsule.SetActive(true);
+            Cube.SetActive(true);
+        }
+    }
+}
